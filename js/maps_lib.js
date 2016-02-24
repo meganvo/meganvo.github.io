ffa934fc7eb3460da4e4033e0f85e3aa4a376273
Megan Vo <meganmvo@gmail.com>
2016-02-23 22:56:42 -0600

    The search function on the map works!
    Lol about time

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -45,7 +45,11 @@
         this.myOptions = {
             zoom: this.defaultZoom,
             center: this.map_centroid,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI:true,
+            zoomControl:true,
+            scrollwheel: false,
+            styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000045"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000045"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000045"},{"lightness":17}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.stroke","stylers":[{"color":"#000045"},{"lightness":20}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000045"},{"lightness":25}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000045"},{"lightness":25}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000045"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"saturation":0},{"color":"#4d88ea"},{"lightness":0}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000045"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000045"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000045"},{"lightness":17},{"weight":1.2}]}]
         };
         this.geocoder = new google.maps.Geocoder();
         this.map = new google.maps.Map($("#map_canvas")[0], this.myOptions);
