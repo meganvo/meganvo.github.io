24a3649a290551bd56d8c7e6fdf86a5624228f1e
Megan Vo <meganmvo@gmail.com>
2016-03-21 09:24:15 -0500

    Cool huge cursor

--- notetoself/js/index.js
+++ notetoself/js/index.js
@@ -1,3 +1,40 @@
+var headID = document.getElementsByTagName("head")[0];         
+var cssNode = document.createElement('link');
+var mousewidth = 15;
+cssNode.type = 'text/css';
+cssNode.rel = 'stylesheet';
+cssNode.media = 'screen';
+headID.appendChild(cssNode);
+
+
+var image = new Image();
+
+image.onLoad = function() {
+
+
+}
+
+image.src="http://s16.postimg.org/o3fq9yfsl/Cursor.png";
+
+image.style.position = "absolute";
+image.style.top = "0";
+image.style.left = "0";
+image.style.zIndex = "999999";
+
+
+var body = document.getElementsByTagName("body")[0];
+
+body.appendChild(image);
+
+document.onmousemove = function(e) {
+    image.style.top = e.clientY + (window.pageYOffset || document.documentElement.scrollTop) + "px";
+    image.style.left = (e.clientX  + 2) + "px";
+}
+
+
+
+
+
     function pickimg(){
     var imagenumber = 123 ;
     var randomnumber = Math.random() ;
