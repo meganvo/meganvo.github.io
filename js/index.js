f54730f5f848d933917a49d666959d3196d684e8
Megan Vo <meganmvo@gmail.com>
2017-02-14 14:34:42 -0600

    Scroll to and Jump

--- js/index.js
+++ js/index.js
@@ -22,3 +22,42 @@ $(document).ready(function() {
         }, 700);
         return false;
     });
+
+//Jump to
+$.fn.scrollView = function () {
+  return this.each(function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 500);
+  });
+}
+
+//Jump to Profile
+$('.about').click(function (event) {
+  event.preventDefault();
+  $('.about-section').scrollView();
+});
+
+//Jump to RigUp Case Study
+
+$('.ru_process').click(function (event) {
+  event.preventDefault();
+  $('#process').scrollView();
+});
+
+$('.ru_analysis').click(function (event) {
+  event.preventDefault();
+  $('#comparison').scrollView();
+});
+
+$('.ru_pattern').click(function (event) {
+  event.preventDefault();
+  $('#pattern_library').scrollView();
+});
+
+$('.ru_branding').click(function (event) {
+  event.preventDefault();
+  $('#marketing').scrollView();
+});
+
+
