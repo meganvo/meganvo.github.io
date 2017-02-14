520755a5054e8cf783f5c674c9e3f4f20a551891
Megan Vo <meganmvo@gmail.com>
2017-02-14 01:29:41 -0600

    Add back to top

--- js/index.js
+++ js/index.js
@@ -23,3 +23,19 @@ $(document).ready(function() {
 			$(".vendor_profile").toggle();
 		});
 	});
+
+//Back to Top
+    var amountScrolled = 150;
+    $(window).scroll(function() {
+        if ( $(window).scrollTop() > amountScrolled ) {
+            $('a.back-to-top').fadeIn('slow');
+        } else {
+            $('a.back-to-top').fadeOut('slow');
+        }
+    });
+    $('a.back-to-top').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 700);
+        return false;
+    });
