3918e2793095ff1877cc2be29281d4130df836ff
Megan Vo <meganmvo@gmail.com>
2017-02-14 01:45:17 -0600

    clean up js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,4 @@
-// Hide show more content initially
-$('.read-more-content').addClass('hide')
-$('.read-more-show, .read-more-hide').removeClass('hide')
 
-// Set up the toggle effect:
-$('.read-more-show').on('click', function(e) {
-  $(this).next('.read-more-content').removeClass('hide');
-  $('.read-more-show').addClass('hide');
-  e.preventDefault();
-});
-
-// Changes contributed by @diego-rzg
-$('.read-more-hide').on('click', function(e) {
-  var p = $(this).parent('.read-more-content');
-  p.addClass('hide');
-  p.prev('.read-more-show').removeClass('hide'); // Hide only the preceding "Read More"
-  e.preventDefault();
-});
 
 // Show vendor profile
 $(document).ready(function() {
